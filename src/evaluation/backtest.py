722b9147bd92a5ad1c767b668a16b5b8e3c0e445
Thien <thiennguyen1993@gmail.com>
2022-10-24 15:55:07 +0800

    Add tests

--- src/evaluation/backtest.py
+++ src/evaluation/backtest.py
@@ -1,4 +1,4 @@
-from itertools import combinations
+from itertools import combinations, chain
 
 import numpy as np
 from scipy.stats import gaussian_kde
@@ -7,7 +7,7 @@ from evaluation import sharpe_ratio
 from utils.stats import KDEDist
 
 
-def compute_pbo(perm_matrix: np.ndarray, eval_fn=sharpe_ratio):
+def compute_pbo(perm_matrix: np.ndarray, eval_fn=sharpe_ratio, n_partitions=10):
     """
     Compute the probability of backtest overfitting for an optimization procedure used to select
     strategies or model configuration. See Bailey et al. [2017] for the original paper.
@@ -15,14 +15,21 @@ def compute_pbo(perm_matrix: np.ndarray, eval_fn=sharpe_ratio):
     :param perm_matrix: an TxN matrix of performance metrics (e.g. returns or PnL) matrix over
         T observations for N different strategies or model configurations
     :param eval_fn: aggregate performance metrics function over a set of metrics observations
+    :param n_partitions: number of partitions to split observations before combining them combinatorially
+        into train and test sets. Must be an even number
     :return: probability, rank logits, train sets metrics, test sets associated mtrics
     """
+    assert n_partitions % 2 == 0
     n_obs, n_strats = perm_matrix.shape
+    partition_size = n_obs // n_partitions
     rank_logits = []
     train_optimal_perm = []
     test_assoc_perm = []
 
-    for train_indices in combinations(range(n_obs), n_obs // 2):
+    for partitions in combinations(range(n_partitions), n_partitions // 2):
+        train_indices = list(chain.from_iterable(
+            [list(range(int(p*partition_size), int((p+1)*partition_size))) for p in partitions]
+        ))
         test_indices = [i for i in range(n_obs) if i not in train_indices]
         train_set = perm_matrix[train_indices, :]
         test_set = perm_matrix[test_indices, :]
@@ -38,7 +45,7 @@ def compute_pbo(perm_matrix: np.ndarray, eval_fn=sharpe_ratio):
             if i == train_optimal_idx:
                 test_rank = r
         test_rank /= test_perm.shape[0]
-        test_rank_logit = np.log(test_rank/(1 - test_rank))
+        test_rank_logit = np.log(test_rank/(1 - test_rank) + 1e-3)
         rank_logits.append(test_rank_logit)
 
     kde = gaussian_kde(rank_logits)
---
+++ test/test_evaluation/test_backtest.py
@@ -0,0 +1,43 @@
+import pytest
+import numpy as np
+from scipy.stats import norm
+
+from evaluation.backtest import compute_pbo
+
+
+@pytest.fixture
+def perm_matrix_norm_rets():
+    np.random.seed(42)
+    n_strats = 10
+    n_obs = 300
+    mean_ret = 0.08
+    std_ret = 0.15
+    return norm.rvs(size=(n_obs, n_strats), loc=mean_ret, scale=std_ret, random_state=42)
+
+
+@pytest.fixture
+def perm_matrix_optimal():
+    np.random.seed(42)
+    n_strats = 10
+    n_obs = 300
+    mean_ret = 0.08
+    std_ret = 0.15
+    optimal_strat_idx = 7
+    optimal_strat_mean = 0.1
+    optimal_strat_std = 0.12
+    out = norm.rvs(size=(n_obs, n_strats), loc=mean_ret, scale=std_ret, random_state=42)
+    out[:, optimal_strat_idx] = norm.rvs(size=n_obs, loc=optimal_strat_mean, scale=optimal_strat_std, random_state=42)
+    return out
+
+
+def test_compute_pbo_norm_rets(perm_matrix_norm_rets):
+    pbo, rank_logits, train_optimal_perm, test_assoc_perm = compute_pbo(perm_matrix_norm_rets, n_partitions=16)
+    assert len(rank_logits) == 12870
+    assert len(train_optimal_perm) == 12870
+    assert len(test_assoc_perm) == 12870
+    assert pbo >= 0.5
+
+
+def test_compute_pbo_optimal(perm_matrix_optimal):
+    pbo, _, _, _ = compute_pbo(perm_matrix_optimal, n_partitions=16)
+    assert pbo < 0.1
